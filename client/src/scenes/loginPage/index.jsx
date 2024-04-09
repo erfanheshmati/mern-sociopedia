3181d69bdd8e77817ed71c6f4acaef3568ef2adc
Erfan Heshmati <heshmati74@gmail.com>
2024-04-09 18:55:52 +0330

    register and login design page with form functionality

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,26 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import Form from "./Form"
+
 export default function LoginPage() {
+    const theme = useTheme()
+    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
+
     return (
-        <div>
-            login page
-        </div>
+        <Box>
+            <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+                <Typography fontWeight="bold" fontSize="32px" color="primary">
+                    Sociopedia
+                </Typography>
+            </Box>
+
+            <Box width={isNonMobileScreen ? "50%" : "93%"} p="2rem" m="2rem auto" borderRadius="1.5rem" backgroundColor={theme.palette.background.alt}>
+                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+                    Welcome to Sociopedia, the Social Media for Sociopaths!
+                </Typography>
+
+                <Form />
+
+            </Box>
+        </Box>
     )
 }
